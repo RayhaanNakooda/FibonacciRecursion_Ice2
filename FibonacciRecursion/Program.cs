9e8de465d205a92c5a03006be87761e80e60c097
Rayhaan <rayhaannak1@gmail.com>
2021-10-18 11:54:04 +0200

    first commit

---
+++ FibonacciRecursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        public static int FibonacciSeries(int n)
+        {
+            if (n == 0) return 0; //To return the first Fibonacci number   
+            if (n == 1) return 1; //To return the second Fibonacci number   
+            return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
